057bb7c79f4b62b971d6727df8f830ff9cb7ac6a
lakshya-02 <lakshya.singh2706@gmail.com>
2024-12-19 23:10:57 +0530

    Sprites added and script modified

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -11,18 +11,41 @@ public class GameOver : MonoBehaviour
 
     private void Start()
     {
+        UpdateScores();
+    }
+
+    private void UpdateScores()
+    {
         int currentScore = PlayerPrefs.GetInt("CurrentScore", 0);
         int highScore = PlayerPrefs.GetInt("HighScore", 0);
+
+        // Update high score if current score is higher
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
+
+        // Update UI texts
+        currentScoreText.text = "Score: " + currentScore;
         highScoreText.text = "High Score: " + highScore;
     }
 
     public void PlayGame()
     {
+        // Reset current score before starting new game
+        PlayerPrefs.SetInt("CurrentScore", 0);
+        PlayerPrefs.Save();
         SceneManager.LoadSceneAsync(1); // Load main game scene
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
